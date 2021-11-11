160eda1bd7e410f48e5c97696a057b5c24746b46
ingcrislopam <ingcrislopam@gmail.com>
2021-11-11 10:05:20 -0500

    Se agregan los archivos base del proyecto

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php
+	//Ip de a pc servidor de base de datos
+	define("DB_HOST", "localhost");
+
+	//Nombre de la base de datos
+	define("DB_NAME", "cdc_ceibo_renacer");
+
+	//Usuario de la base de datos
+	define("DB_USERNAME", "root");
+
+	//Contraseña del usuario de la base de datos
+	define("DB_PASSWORD", "");
+
+	//Definimos la codificación de los caracteres
+	define("DB_ENCODE", "utf8");
+
+	//Definimos una constante como nombre del proyecto
+	define("PRO_NOMBRE", "CDC Ceibo Renacer");
+?>
