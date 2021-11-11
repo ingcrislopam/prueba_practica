735e045d3c756963eef4c77826c9aaea49a9596a
ingcrislopam <ingcrislopam@gmail.com>
2021-11-11 10:33:03 -0500

    Se establecieron los parametros para la conexión a la base de datos

--- config/global.php
+++ config/global.php
@@ -1,19 +1,19 @@
 <?php
 	//Ip de a pc servidor de base de datos
-	define("DB_HOST", "localhost");
+	define("DB_HOST", "consulti.ec:9001");
 
 	//Nombre de la base de datos
-	define("DB_NAME", "cdc_ceibo_renacer");
+	define("DB_NAME", "developer");
 
 	//Usuario de la base de datos
 	define("DB_USERNAME", "root");
 
 	//Contraseña del usuario de la base de datos
-	define("DB_PASSWORD", "");
+	define("DB_PASSWORD", "consultidev");
 
 	//Definimos la codificación de los caracteres
 	define("DB_ENCODE", "utf8");
 
 	//Definimos una constante como nombre del proyecto
-	define("PRO_NOMBRE", "CDC Ceibo Renacer");
+	define("PRO_NOMBRE", "Prueba Practica");
 ?>
